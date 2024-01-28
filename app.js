e971974a5b808b358b911fd03f3b73c1528b3bc2
Bruno Brito <bruno@brittus.com>
2024-01-28 19:20:41 -0300

    POST alterado com a chamada a OpenAI

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const { OpenAI } = require('openai')
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
 
 app.use(express.json())
 
-app.post('/ask-openai', (req, res) => {
+app.post('/ask-openai', async (req, res) => {
     try {
         let content = req.body.question || 'Ola'
-        res.json({ response: content })      
+        const response = await openai.chat.completions.create({
+            model: "gpt-4-1106-preview",
+            response_format: {"type": "json_object"},
+            messages: [
+                { role: "system", content: "You are a helpful assistant designed to output JSON." },
+                { role: "user", content: content}
+            ],
+            temperature: 2,
+            max_tokens: 150,
+            top_p: 0,
+            frequency_penalty: 1,
+            presence_penalty: 1,
+        })
+        res.send(response.choices[0].message.content)      
     } catch (error) {
+        console.error(error);
         res.status(500).json({ error: error.message })
     }
 })
